0d5f0cb4c3e576c3a57adcaf8f99d2935111bcaa
Jin Zhang <jinzh001@gmail.com>
2018-03-12 01:33:22 -0700

    Lesson 19 excercise, done with svm training

---
+++ devel/share/sensor_stick/cmake/sensor_stick-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(sensor_stick_MESSAGE_FILES "/home/robond/catkin_ws/src/msg/DetectedObject.msg;/home/robond/catkin_ws/src/msg/DetectedObjectsArray.msg")
+set(sensor_stick_SERVICE_FILES "/home/robond/catkin_ws/src/srv/GetNormals.srv;/home/robond/catkin_ws/src/srv/GetFloatArrayFeature.srv")
